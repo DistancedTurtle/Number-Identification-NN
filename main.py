e574a64781e4d13e81e55d37ac5cfaf93b5c461e
Nick <nickpearlst@gmail.com>
2026-06-08 19:07:23 -0500

    Stop tracking DS_Store/opt/homebrew/bin/python3

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ transform = transforms.Compose([transfrom.ToTensor])
 
 train = torchvision.datasets.MNIST(root='./data', train=True, download=true, transform=transform)
 test = torchvision.datasets.MNIST(root='./data', train=False, download=true, transform=transform)
+
+X_train = train.data.numpy()
+X_test = test.data.numpy()
+
+y_train = train.targets.numpy()
+y_test = test.targets.numpy()
+
+print(f"Train images: {X_train.shape}, type: {type(X_train)}")
+print(f"Train labels: {y_train.shape}, type: {type(y_train)}")
+
